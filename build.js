3be1bec9a420e22efa2c29f9baf2c9c3fce445e2
ybouane <yassine@bouanane.com>
2025-01-22 01:25:39 -0500

    Bundling for dist

---
+++ build.js
@@ -0,0 +1,21 @@
+import { build } from 'esbuild';
+import { promises as fs } from "fs";
+
+
+(async () => {
+	try {
+		await build({
+			entryPoints: ['index.js'], // Entry point of your library
+			outdir: 'dist', // Output directory
+			bundle: true, // Bundle all files
+			minify: true, // Minify the output
+			sourcemap: true, // Generate source maps
+			format: 'esm', // Output format (ES Module)
+		});
+		await fs.copyFile('./libraw.wasm', './dist/libraw.wasm');
+		console.log('Build successful!');
+	} catch (error) {
+		console.error('Build failed:', error);
+		process.exit(1);
+	}
+})();
