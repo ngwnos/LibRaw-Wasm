218eeeccb1964e94747fa9e0505849d003e3699a
ybouane <yassine@bouanane.com>
2025-01-28 14:18:10 -0500

    Refactor build system to be more streamlined and reproducable

--- build.js
+++ build.js
@@ -4,6 +4,9 @@ import { promises as fs } from "fs";
 
 (async () => {
 	try {
+		let librawjs = (await fs.readFile('./libraw.js')).toString();
+		librawjs = librawjs.replace(/var workerOptions=([^]+?);worker=new Worker\(new URL\("([^"]+)",import.meta.url\),workerOptions\);/, `worker=new Worker(new URL("$2",import.meta.url),$1);`); // Correction to make worker options static so that it works with vite
+		await fs.writeFile('./libraw.js', librawjs);
 		await build({
 			entryPoints: ['index.js', 'worker.js'], // Entry point of your library
 			outdir: 'dist', // Output directory
